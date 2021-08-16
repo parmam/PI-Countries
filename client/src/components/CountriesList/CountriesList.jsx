ce4d212e5620e7b78e4009b5fe349dd4922cfcae
GotoxyDev <contact@gotoxy.dev>
2021-08-15 23:45:17 -0300

    aasdsaddddd

--- client/src/components/CountriesList/CountriesList.jsx
+++ client/src/components/CountriesList/CountriesList.jsx
@@ -1,125 +1,20 @@
 import styles from './CountriesList.module.css';
 import React,{useState, useEffect} from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { getCountries, orderBy, allFilters } from '../../redux/actions';
+import { useSelector } from 'react-redux';
 import CountryCard from '../CountryCard/CountryCard';
 
 const CountriesList = () => {
     const allCountries = useSelector(store => store.allCountries)
-    const filters = useSelector(store=> store.allFilters)
-    const dispatch = useDispatch()
-    const [flag, setFlag] = useState(1)
+    const allFilters = useSelector(store => store.allFilters)
     const [countries, setCountries] = useState([])
-    const [contienentFilter, setContinentFilter] = useState('DEFAULT')
-    const [activityFilter, setActivityFilter] = useState('DEFAULT')
-    const [populationOrder, setPopulationOrder] = useState('DEFAULT')
-    const [alphabethicOrder, setAlphabethicOrder] = useState('A-Z')
-
 
     useEffect(() =>{
-        if(flag === 1){
-            dispatch(getCountries())
-            setFlag(0)
-        }
-        if(flag === 2){
-            dispatch(orderBy(alphabethicOrder, populationOrder))
-            dispatch(allFilters(activityFilter, contienentFilter))
-            setFlag(0)
-        }
         setCountries(allCountries)
+    },[countries, allCountries, allFilters])
 
-    },[dispatch, countries, allCountries, alphabethicOrder, populationOrder, activityFilter, contienentFilter, filters])
-
-
-
-    const orderByAz = (e) => {
-        setAlphabethicOrder(e.target.value)
-        setPopulationOrder('DEFAULT')
-        document.getElementById('POPULATION').value = 'DEFAULT'
-
-        console.log(populationOrder)
-        console.log(alphabethicOrder)
-        setFlag(2)
-    }
-
-    const orderByPopulation = (e) => {
-        setPopulationOrder(e.target.value)
-        setAlphabethicOrder('DEFAULT')
-        document.getElementById('AZ').value = 'DEFAULT'
-        console.log(populationOrder)
-        setFlag(2)
-    }
-
-    const filterByActivity = (e) => {
-        setActivityFilter(e.target.value)
-        console.log(activityFilter)
-
-        setFlag(2)
-    }
-
-    const filterByContinent = (e) => {
-        setContinentFilter(e.target.value)
-
-        console.log(contienentFilter)
-
-        setFlag(2)
-    }
-
-
-
- 
 
     return(
-        <React.Fragment >
-            <div className={styles.filtersCtn}>
-                <select 
-                name="" 
-                id="AZ" 
-                className={styles.filterBtn}
-                onChange={(e) => orderByAz(e)}
-                >
-                    <option value="DEFAULT">DEFAULT</option>
-                    <option value="A-Z">A-Z</option>
-                    <option value="Z-A">Z-A</option>
-                </select>
-                
-                <select 
-                onChange="" 
-                id="POPULATION" 
-                className={styles.filterBtn}
-                onChange={(e) => orderByPopulation(e)}
-                >
-                    <option value="DEFAULT">DEFAULT</option>
-                    <option value="ASC">ASC</option>
-                    <option value="DESC">DESC</option>
-                </select>
-
-                <select 
-                name="" 
-                id="" 
-                className={styles.filterBtn}
-                onChange={(e) => filterByContinent(e)}
-                >
-                    <option value="DEFAULT">ALL CONTINENTS</option>
-                    <option value="AFRICA">AFRICA</option>
-                    <option value="AMERICA">AMERICA</option>
-                    <option value="ASIA">ASIA</option>
-                    <option value="EUROPE">EUROPE</option>
-                    <option value="OCAENIA">OCEANIA</option>
-                </select>
-
-                <select 
-                name="" 
-                id="" 
-                className={styles.filterBtn}
-                onChange={(e) => filterByActivity(e)}
-                >
-                    <option value="DEFAULT">DEFAULT</option>
-                </select>
-            </div>
-
-
-
+        <React.Fragment>
             <div className={styles.cardContainer}>
                 {(!countries.length) 
                 ? <p>loading countries...</p>
--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -1,7 +1,7 @@
 import styles from './SearchForm.module.css'
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { getCountries } from '../../redux/actions';
+import { getCountries, allFilters, orderBy } from '../../redux/actions';
 import { useModal } from '../../hooks/useModal';
 import Modal from '../Modal/Modal';
 import AddActivities from '../AddActivities/AddActivities';
@@ -10,7 +10,14 @@ import AddActivities from '../AddActivities/AddActivities';
 const SearchForm = () => {
 
     const [isOpenActivitiesModal, openActivitiesModal, closeActivitiesModal] = useModal(false)
+    const [contienentFilter, setContinentFilter] = useState('DEFAULT')
+    const [activityFilter, setActivityFilter] = useState('DEFAULT')
+    const [populationOrder, setPopulationOrder] = useState('DEFAULT')
+    const [alphabethicOrder, setAlphabethicOrder] = useState('A-Z')
     const [search, setSearch] = useState('')
+    const [countries, setCountries] = useState([])
+    const [flag, setFlag] = useState(1)
+    const allCountries = useSelector(store => store.allCountries)
     const filters = useSelector(store=> store.allFilters)
     const dispatch = useDispatch()
 
@@ -21,6 +28,51 @@ const SearchForm = () => {
     const handleChange = (e) => {
         setSearch(e.target.value)
     }
+
+
+
+    const orderByAz = (e) => {
+        setAlphabethicOrder(e.target.value)
+        setPopulationOrder('DEFAULT')
+        document.getElementById('POPULATION').value = 'DEFAULT'
+        console.log(populationOrder)
+        console.log(alphabethicOrder)
+        setFlag(2)
+    }
+
+    const orderByPopulation = (e) => {
+        setPopulationOrder(e.target.value)
+        setAlphabethicOrder('DEFAULT')
+        document.getElementById('AZ').value = 'DEFAULT'
+        console.log(populationOrder)
+        setFlag(2)
+    }
+
+    const filterByActivity = (e) => {
+        setActivityFilter(e.target.value)
+        console.log(activityFilter)
+        setFlag(2)
+    }
+
+    const filterByContinent = (e) => {
+        setContinentFilter(e.target.value)
+        console.log(contienentFilter)
+        setFlag(2)
+    }
+
+
+    useEffect(() => {
+        if(flag === 2){
+            dispatch(orderBy(alphabethicOrder, populationOrder))
+            dispatch(allFilters(activityFilter, contienentFilter))
+            setFlag(0)
+        }
+        if(flag === 1) {
+            dispatch(getCountries())
+            setFlag(0)
+        }
+        setCountries(allCountries)
+    },[dispatch, flag, alphabethicOrder, populationOrder, activityFilter, contienentFilter, allCountries, countries])
     
     return (
         <React.Fragment>
@@ -49,6 +101,52 @@ const SearchForm = () => {
                         
                     </div>
                 </form>
+            </div>
+            <div className={styles.filtersCtn}>
+                <select 
+                name="" 
+                id="AZ" 
+                className={styles.filterBtn}
+                onChange={(e) => orderByAz(e)}
+                >
+                    <option value="DEFAULT">DEFAULT</option>
+                    <option value="A-Z">A-Z</option>
+                    <option value="Z-A">Z-A</option>
+                </select>
+                
+                <select 
+                onChange="" 
+                id="POPULATION" 
+                className={styles.filterBtn}
+                onChange={(e) => orderByPopulation(e)}
+                >
+                    <option value="DEFAULT">DEFAULT</option>
+                    <option value="ASC">ASC</option>
+                    <option value="DESC">DESC</option>
+                </select>
+
+                <select 
+                name="" 
+                id="" 
+                className={styles.filterBtn}
+                onChange={(e) => filterByContinent(e)}
+                >
+                    <option value="DEFAULT">ALL CONTINENTS</option>
+                    <option value="AFRICA">AFRICA</option>
+                    <option value="AMERICA">AMERICA</option>
+                    <option value="ASIA">ASIA</option>
+                    <option value="EUROPE">EUROPE</option>
+                    <option value="OCAENIA">OCEANIA</option>
+                </select>
+
+                <select 
+                name="" 
+                id="" 
+                className={styles.filterBtn}
+                onChange={(e) => filterByActivity(e)}
+                >
+                    <option value="DEFAULT">DEFAULT</option>
+                </select>
             </div>
         </React.Fragment>
     )
